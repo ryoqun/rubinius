104ae0505b420f4f800c777a47a8664c358b3425
Ryo Onodera <ryoqun@gmail.com>
2013-10-02 13:03:58 +0900

    Add member functions for creating OptimizedCallSite

--- vm/builtin/optimized_call_site.cpp
+++ vm/builtin/optimized_call_site.cpp
@@ -8,6 +8,19 @@ namespace rubinius {
     G(optimized_call_site)->set_object_type(state, OptimizedCallSiteType);
   }
 
+  OptimizedCallSite* OptimizedCallSite::create(STATE, CallSite* unoptimized) {
+    OptimizedCallSite* call_site =
+      state->vm()->new_object_mature<OptimizedCallSite>(G(optimized_call_site));
+    call_site->name_     = unoptimized->name();
+    call_site->executable(state, unoptimized->executable());
+    call_site->ip_       = unoptimized->ip();
+    call_site->executor_ = empty_cache;
+    call_site->fallback_ = unoptimized->fallback_;
+    call_site->updater_  = empty_cache_updater;
+    call_site->fallback_call_site(state, unoptimized);
+    return call_site;
+  }
+
   void OptimizedCallSite::Info::mark(Object* obj, ObjectMark& mark) {
     auto_mark(obj, mark);
   }
