404b8afbc7ea703c2134eaa7855805416ff23506
Ryo Onodera <ryoqun@gmail.com>
2013-10-02 12:41:38 +0900

    Define OptimizedCallSite

---
+++ vm/builtin/optimized_call_site.hpp
@@ -0,0 +1,23 @@
+#ifndef RBX_OPTIMIZED_CALL_SITE_HPP
+#define RBX_MONO_INLINE_CACHE_HPP
+
+#include "builtin/call_site.hpp"
+#include "ontology.hpp"
+
+namespace rubinius {
+  class OptimizedCallSite : public CallSite {
+  public:
+    const static object_type type = OptimizedCallSiteType;
+    static void init(STATE);
+    static OptimizedCallSite* create(STATE, CallSite* call_site);
+
+  public: // Rubinius Type stuff
+    class Info : public CallSite::Info {
+    public:
+      BASIC_TYPEINFO(CallSite::Info)
+      virtual void mark(Object* t, ObjectMark& mark);
+    };
+  };
+}
+
+#endif
